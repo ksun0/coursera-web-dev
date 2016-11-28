4ef803db6b98f5de4ba0970c777d6cfcd27b9aeb
ksun0 <kevinsun0@gmail.com>
2016-11-27 21:47:00 -0500

    fixed image

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,88 +1,88 @@
 (function() {
-'use strict';
+  'use strict';
 
-angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController', NarrowItDownController)
-.service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
-.directive('foundItems', foundItems);
-
-
-function foundItems(){
-  var ddo = {
-    templateUrl: 'foundItems.html',
-    restrict: 'E',
-    scope: {
-      foundItems: '<',
-      myMessage: '@message',
-      onRemove: '&'
-    },
-    controller: NarrowItDownDirectiveController,
-    controllerAs: 'ctrl',
-    bindToController: true
-
-  };
-  return ddo;
-};
-
-function NarrowItDownDirectiveController() {
-  var ctrl = this;
-};
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
+  .directive('foundItems', foundItems);
 
 
-NarrowItDownController.$inject = ['MenuSearchService'];
-function NarrowItDownController(MenuSearchService) {
-  var ctrl = this;
+  function foundItems(){
+    var ddo = {
+      templateUrl: 'foundItems.html',
+      restrict: 'E',
+      scope: {
+        foundItems: '<',
+        myMessage: '@message',
+        onRemove: '&'
+      },
+      controller: NarrowItDownDirectiveController,
+      controllerAs: 'ctrl',
+      bindToController: true
 
-  ctrl.showSearchedItems = function() {
-
-    ctrl.message = 'loading...';
-
-    var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
-
-    promise.then(function(response) {
-      ctrl.found = response;
-
-      if (ctrl.found.length) {
-        ctrl.message = 'Found ' + ctrl.found.length + ' item(s)';
-      } else {
-        ctrl.message = 'Nothing found'
-      }
-    })
+    };
+    return ddo;
   };
 
-  ctrl.removeItem = function(index){
-    ctrl.found.splice(index, 1);
-    ctrl.message = 'Found ' + ctrl.found.length + ' item(s)';
+  function NarrowItDownDirectiveController() {
+    var ctrl = this;
   };
-};
 
 
-MenuSearchService.$inject = ['$http', 'ApiBasePath'];
-function MenuSearchService($http, ApiBasePath) {
-  var service = this;
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var ctrl = this;
 
-  service.getMatchedMenuItems = function(searchTerm){
+    ctrl.showSearchedItems = function() {
 
-    return $http({
-      method: 'GET',
-      url: (ApiBasePath + '/menu_items.json')
-    })
-    .then(function(result){
-      var foundItems = [];
-      if (searchTerm !== ''){
-        result.data.menu_items.forEach(function (item) {
-          if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
-            foundItems.push(item);
-          }
-        });
-      }
+      ctrl.message = 'loading...';
 
-      return foundItems;
-    });
+      var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
 
+      promise.then(function(response) {
+        ctrl.found = response;
+
+        if (ctrl.found.length) {
+          ctrl.message = 'Found ' + ctrl.found.length + ' item(s)';
+        } else {
+          ctrl.message = 'Nothing found'
+        }
+      })
+    };
+
+    ctrl.removeItem = function(index){
+      ctrl.found.splice(index, 1);
+      ctrl.message = 'Found ' + ctrl.found.length + ' item(s)';
+    };
   };
-};
+
+
+  MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+  function MenuSearchService($http, ApiBasePath) {
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm){
+
+      return $http({
+        method: 'GET',
+        url: (ApiBasePath + '/menu_items.json')
+      })
+      .then(function(result){
+        var foundItems = [];
+        if (searchTerm !== ''){
+          result.data.menu_items.forEach(function (item) {
+            if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
+              foundItems.push(item);
+            }
+          });
+        }
+
+        return foundItems;
+      });
+
+    };
+  };
 
 
 })();
